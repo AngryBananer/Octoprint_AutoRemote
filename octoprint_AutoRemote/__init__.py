64868388d2ee64fdca2e9efa130d626a5ce1be85
AngryBananer <15734119+AngryBananer@users.noreply.github.com>
2020-12-21 21:12:53 +0100

    Added PrintProgress Event

--- octoprint_AutoRemote/__init__.py
+++ octoprint_AutoRemote/__init__.py
@@ -8,22 +8,25 @@ from octoprint.events import Events
 
 import os
 
-#TODO LIST
-#Add progress for % or Layer and textbox for value
-#Add URL field for each trigger to make it a general webhook plugin instead of just AutoRemote
+# TODO LIST
+# Add progress for % or Layer and textbox for value
+# Add URL field for each trigger to make it a general webhook plugin instead of just AutoRemote
+
 
 class OctoAutoremotePlugin(octoprint.plugin.StartupPlugin,
-                        octoprint.plugin.TemplatePlugin,
-                        octoprint.plugin.SettingsPlugin,
-                        octoprint.plugin.EventHandlerPlugin):
+                           octoprint.plugin.TemplatePlugin,
+                           octoprint.plugin.SettingsPlugin,
+                           octoprint.plugin.EventHandlerPlugin,
+                           octoprint.plugin.ProgressPlugin):
 
     def on_settings_save(self, data):
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
         if (hasattr, self._settings, 'autoremotekey'):
-#            self.autoremotekey = self._settings.get(["autoremotekey"])
-            self._logger.info("Saving AutoRemote personal Key: %s" % self._settings.get(["autoremotekey"]))
+            #            self.autoremotekey = self._settings.get(["autoremotekey"])
+            self._logger.info("Saving AutoRemote personal Key: %s" %
+                              self._settings.get(["autoremotekey"]))
         else:
-#            self.autoremotekey=''
+            #            self.autoremotekey=''
             self._logger.info("No Autoremote Personal key set while trying to save!")
 
     def on_after_startup(self):
@@ -33,51 +36,52 @@ class OctoAutoremotePlugin(octoprint.plugin.StartupPlugin,
 
     def get_settings_defaults(self):
         return dict(autoremotekey="",
-            autoremotesender="",
-            autoremotepassword="",
+                    autoremotesender="",
+                    autoremotepassword="",
                     events=dict(Startup=False
-                                        ,Shutdown=False
-                                        ,ClientOpened=False
-                                        ,ClientClosed=False
-                                        ,ConnectivityChanged=False
-                                        ,Connecting=False
-                                        ,Connected=False
-                                        ,PrintStarted=False
-                                        ,Disconnecting=False
-                                        ,Disconnected=False
-                                        ,Error=False
-                                        ,PrinterStateChanged=False
-                                        ,Upload=False
-                                        ,FileAdded=False
-                                        ,FileRemoved=False
-                                        ,FolderAdded=False
-                                        ,FolderRemoved=False
-                                        ,UpdatedFiles=False
-                                        ,MetadataAnalysisStarted=False
-                                        ,MetadataAnalysisFinished=False
-                                        ,FileSelected=False
-                                        ,FileDeselected=False
-                                        ,TransferStarted=False
-                                        ,TransferDone=False
-                                        ,PrintFailed=False
-                                        ,PrintCancelling=False
-                                        ,PrintCancelled=False
-                                        ,PrintPaused=False
-                                        ,PrintResumed=False
-                                        ,PrintDone=False
-                                        ,MovieRendering=False
-                                        ,MovieDone=False
-                                        ,MovieFailed=False
-                                        ,CaptureStart=False
-                                        ,CaptureDone=False
-                                        ,CaptureFailed=False
-                                        ,SettingsUpdated=False
-                                        )
-                   )
-
+                                    ,Shutdown=False
+                                    ,ClientOpened=False
+                                    ,ClientClosed=False
+                                    ,ConnectivityChanged=False
+                                    ,Connecting=False
+                                    ,Connected=False
+                                    ,PrintStarted=False
+                                    ,Disconnecting=False
+                                    ,Disconnected=False
+                                    ,Error=False
+                                    ,PrinterStateChanged=False
+                                    ,Upload=False
+                                    ,FileAdded=False
+                                    ,FileRemoved=False
+                                    ,FolderAdded=False
+                                    ,FolderRemoved=False
+                                    ,UpdatedFiles=False
+                                    ,MetadataAnalysisStarted=False
+                                    ,MetadataAnalysisFinished=False
+                                    ,FileSelected=False
+                                    ,FileDeselected=False
+                                    ,TransferStarted=False
+                                    ,TransferDone=False
+                                    ,PrintFailed=False
+                                    ,PrintCancelling=False
+                                    ,PrintCancelled=False
+                                    ,PrintPaused=False
+                                    ,PrintResumed=False
+                                    ,PrintDone=False
+                                    ,MovieRendering=False
+                                    ,MovieDone=False
+                                    ,MovieFailed=False
+                                    ,CaptureStart=False
+                                    ,CaptureDone=False
+                                    ,CaptureFailed=False
+                                    ,SettingsUpdated=False
+                                ),
+                    PrintProgress=False,
+                    PrintProgressSteps="5"
+                    )
 
     def get_template_configs(self):
-        return [ dict(type="settings", name="OctoAutoremote", custom_bindings=False) ]
+        return [dict(type="settings", name="OctoAutoremote", custom_bindings=False)]
 
     def get_assets(self):
         return dict(
@@ -90,25 +94,36 @@ class OctoAutoremotePlugin(octoprint.plugin.StartupPlugin,
 
 ######
 
+    def on_print_progress(self, storage, path, progress):
+        if self._settings.get(['PrintProgress']) and progress % int(self._settings.get(['PrintProgressSteps'])) == 0:
+            #self._logger.info("PrintProgress: %s" % (progress))
+            payload = {}
+            payload['storage'] = storage
+            payload['path'] = path
+            payload['progress'] = str(progress)
+            self.on_event(event='PrintProgress', payload=payload)
+
     def on_event(self, event, payload):
         events = self._settings.get(['events'], merged=True)
 
-        if event in events and events[event]:
-             messagedata = {}
-             messagedata['event'] = event
+        if event in events and events[event] or event=='PrintProgress':
+            messagedata = {}
+            messagedata['event'] = event
 
-             if not payload:
-                 payload = {}
-                 messagedata['nodata'] = 'No_Data_For_This_Event'
-             else:
-                 for data in payload:
-                     messagedata[str(data).lower()] = str(payload[data]).replace("::ffff:", "")
-                     self._logger.debug("forming_Message: '%s':'%s'" % (str(data).lower(), str(payload[data]).replace("::ffff:", "")))
+            if not payload:
+                payload = {}
+                messagedata['nodata'] = 'No_Data_For_This_Event'
+            else:
+                for data in payload:
+                    messagedata[str(data).lower()] = str(
+                        payload[data]).replace("::ffff:", "")
+                    self._logger.debug("forming_Message: '%s':'%s'" % (
+                        str(data).lower(), str(payload[data]).replace("::ffff:", "")))
 
-             message = 'OctoAutoremote=:=' + json.dumps(messagedata)
+            message = 'OctoAutoremote=:=' + json.dumps(messagedata)
 
-             self._logger.info("Calling Send: Event: %s, Message: %s" % (event, message))
-             self._send_AutoRemote(message)
+            self._logger.info("Calling Send: Event: %s, Message: %s" % (event, message))
+            self._send_AutoRemote(message)
 #        else:
 #             self._logger.info("Event skipped: %s" % event)
 
@@ -121,19 +136,19 @@ class OctoAutoremotePlugin(octoprint.plugin.StartupPlugin,
 
         url = "https://autoremotejoaomgcd.appspot.com/sendrequest"
         messageObj = {
-                 'message': message,
-                 'sender': autoremotesender,
-                 'password': autoremotepassword,
-                 'communication_base_params': {
-                      'sender': autoremotesender,
-                      'type': 'Message'
-                      }
+            'message': message,
+            'password': autoremotepassword,
+            'sender': autoremotesender,
+            'communication_base_params': {
+                'sender': autoremotesender,
+                'type': 'Message'
+            }
         }
 
         dataObj = {
-             'key': autoremotekey,
-             'sender': autoremotesender,
-             'request': json.dumps(messageObj)
+            'key': autoremotekey,
+            'sender': autoremotesender,
+            'request': json.dumps(messageObj)
         }
 
         self._logger.info("Sending %s to URL: %s" % (dataObj, url))
@@ -153,10 +168,11 @@ class OctoAutoremotePlugin(octoprint.plugin.StartupPlugin,
                 repo="octoprint_AutoRemote",
                 stable_branch=dict(branch="master", name="Stable"),
                 pip="https://github.com/trunzoc/octoprint_Autoremote/archive/{target_version}.zip"
-                )
             )
+        )
 
 ######
+
 
 __plugin_name__ = "OctoAutoremote"
 __plugin_implementation__ = OctoAutoremotePlugin()
@@ -164,5 +180,5 @@ __plugin_pythoncompat__ = ">=2.7,<4"
 
 global __plugin_hooks__
 __plugin_hooks__ = {
-                "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-                    }
+    "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+}
